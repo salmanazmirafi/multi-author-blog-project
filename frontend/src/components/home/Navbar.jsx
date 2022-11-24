4eb35cd46458d182693ec37484b225f8e8f2962e
Salman Azmi Rafi <salmanazmirafipersonal@gmail.com>
2022-11-25 01:22:59 +0600

    some modify

--- frontend/src/components/home/Navbar.jsx
+++ frontend/src/components/home/Navbar.jsx
@@ -1,9 +1,82 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import {
+  FaFacebookSquare,
+  FaTwitterSquare,
+  FaYoutube,
+  FaGithubSquare,
+} from "react-icons/fa";
+import { ImLinkedin } from "react-icons/im";
+import { BsListUl } from "react-icons/bs";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div id="navbar" className="navbar">
+      <div className="container">
+        <div className="row">
+          <input type="checkbox" id="toggle" />
+          <div className="col-4">
+            <div className="image-menubar">
+              <Link className="image" to="/">
+                <img src="http://localhost:3000/assects/myBlog.png" alt="" />
+              </Link>
+              <label className="menu_icon" htmlFor="toggle">
+                <BsListUl />
+              </label>
+            </div>
+          </div>
+          <div className="col-8">
+            <ul className="link-list toggle">
+              <li className="link-item">
+                <Link to="/about">About</Link>
+              </li>
+              <li className="link-item">
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li className="link-item">
+                <Link to="/policy">Policy</Link>
+              </li>
+              <div className="social-icon">
+                <li className="link-item">
+                  <Link to="/">
+                    <span>
+                      <FaFacebookSquare />
+                    </span>
+                  </Link>
+                </li>
+                <li className="link-item">
+                  <Link to="/">
+                    <span>
+                      <FaTwitterSquare />
+                    </span>
+                  </Link>
+                </li>
+                <li className="link-item">
+                  <Link to="/">
+                    <span>
+                      <FaYoutube />
+                    </span>
+                  </Link>
+                </li>
+                <li className="link-item">
+                  <Link to="/">
+                    <span>
+                      <FaGithubSquare />
+                    </span>
+                  </Link>
+                </li>
+                <li className="link-item">
+                  <Link to="/">
+                    <span>
+                      <ImLinkedin />
+                    </span>
+                  </Link>
+                </li>
+              </div>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
